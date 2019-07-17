8db7311fe3e89304992aff236c4816d12983ebef
Peter Zhu <peter@peterzhu.ca>
2019-07-17 09:54:26 -0400

    Move some autocorrect tests to a shared block

--- spec/cop/sorbet/valid_sorbet_sigil_spec.rb
+++ spec/cop/sorbet/valid_sorbet_sigil_spec.rb
@@ -7,6 +7,38 @@ require_relative '../../../lib/rubocop/cop/sorbet/valid_sorbet_sigil'
 RSpec.describe(RuboCop::Cop::Sorbet::ValidSorbetSigil, :config) do
   subject(:cop) { described_class.new(config) }
 
+  shared_examples_for 'no autocorrect on files with sigil' do
+    it('does not change files with a sigil') do
+      expect(
+        autocorrect_source(<<~RUBY)
+          # frozen_string_literal: true
+          # typed: strict
+          class Foo; end
+        RUBY
+      )
+        .to(eq(<<~RUBY))
+          # frozen_string_literal: true
+          # typed: strict
+          class Foo; end
+        RUBY
+    end
+
+    it('does not change files with an invalid sigil') do
+      expect(
+        autocorrect_source(<<~RUBY)
+          # frozen_string_literal: true
+          # typed: no
+          class Foo; end
+        RUBY
+      )
+        .to(eq(<<~RUBY))
+          # frozen_string_literal: true
+          # typed: no
+          class Foo; end
+        RUBY
+    end
+  end
+
   describe('RequireSigilOnAllFiles: true') do
     let(:cop_config) do
       {
@@ -58,6 +90,8 @@ RSpec.describe(RuboCop::Cop::Sorbet::ValidSorbetSigil, :config) do
     end
 
     describe('autocorrect') do
+      it_should_behave_like 'no autocorrect on files with sigil'
+
       it('autocorrects by adding typed: false to file without sigil') do
         expect(
           autocorrect_source(<<~RUBY)
@@ -68,36 +102,6 @@ RSpec.describe(RuboCop::Cop::Sorbet::ValidSorbetSigil, :config) do
           .to(eq(<<~RUBY))
             # typed: false
             # frozen_string_literal: true
-            class Foo; end
-          RUBY
-      end
-
-      it('does not change files with a sigil') do
-        expect(
-          autocorrect_source(<<~RUBY)
-            # frozen_string_literal: true
-            # typed: strict
-            class Foo; end
-          RUBY
-        )
-          .to(eq(<<~RUBY))
-            # frozen_string_literal: true
-            # typed: strict
-            class Foo; end
-          RUBY
-      end
-
-      it('does not change files with an invalid sigil') do
-        expect(
-          autocorrect_source(<<~RUBY)
-            # frozen_string_literal: true
-            # typed: no
-            class Foo; end
-          RUBY
-        )
-          .to(eq(<<~RUBY))
-            # frozen_string_literal: true
-            # typed: no
             class Foo; end
           RUBY
       end
@@ -144,6 +148,8 @@ RSpec.describe(RuboCop::Cop::Sorbet::ValidSorbetSigil, :config) do
     end
 
     describe('autocorrect') do
+      it_should_behave_like 'no autocorrect on files with sigil'
+
       it('does not change files without sigil') do
         expect(
           autocorrect_source(<<~RUBY)
@@ -153,36 +159,6 @@ RSpec.describe(RuboCop::Cop::Sorbet::ValidSorbetSigil, :config) do
         )
           .to(eq(<<~RUBY))
             # frozen_string_literal: true
-            class Foo; end
-          RUBY
-      end
-
-      it('does not change files with a sigil') do
-        expect(
-          autocorrect_source(<<~RUBY)
-            # frozen_string_literal: true
-            # typed: strict
-            class Foo; end
-          RUBY
-        )
-          .to(eq(<<~RUBY))
-            # frozen_string_literal: true
-            # typed: strict
-            class Foo; end
-          RUBY
-      end
-
-      it('does not change files with an invalid sigil') do
-        expect(
-          autocorrect_source(<<~RUBY)
-            # frozen_string_literal: true
-            # typed: no
-            class Foo; end
-          RUBY
-        )
-          .to(eq(<<~RUBY))
-            # frozen_string_literal: true
-            # typed: no
             class Foo; end
           RUBY
       end
