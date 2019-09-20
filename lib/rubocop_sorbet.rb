c01dd7824adf7ce4f535e7637d7b91911fde4707
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2019-09-20 15:42:00 -0400

    Refactor (and move) `ValidSorbetSigil` to `sigils/ValidSigil` to facilitate subclassing
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/rubocop_sorbet.rb
+++ lib/rubocop_sorbet.rb
@@ -9,4 +9,5 @@ require_relative 'rubocop/cop/sorbet/forbid_superclass_const_literal'
 require_relative 'rubocop/cop/sorbet/forbid_include_const_literal'
 require_relative 'rubocop/cop/sorbet/parameters_ordering_in_signature'
 require_relative 'rubocop/cop/sorbet/keyword_argument_ordering'
-require_relative 'rubocop/cop/sorbet/valid_sorbet_sigil'
+
+require_relative 'rubocop/cop/sorbet/sigils/valid_sigil'
