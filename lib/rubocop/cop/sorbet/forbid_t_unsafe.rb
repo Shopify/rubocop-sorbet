6c9c6f0b007f661701aa23403a73d522dca60fbe
Kevin Deisz <kevin.deisz@gmail.com>
2021-05-28 09:30:50 -0400

    Add ForbidTUnsafe cop
    I don't think this cop should be enabled by default, as it's a very common and convenient way to bail out of type checks when Sorbet can't figure out what's going on. However, there are times when you want to find all usage of `T.unsafe` in your codebase. For example, when tapioca releases new DSLs that make it so that more things can be typed, a lot of the time that means we can get rid of things that we previously called `T.unsafe`. So this cop is mostly to find and document those usages so that they don't get lost.

---
+++ lib/rubocop/cop/sorbet/forbid_t_unsafe.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+module RuboCop
+  module Cop
+    module Sorbet
+      # This cop disallows using `T.unsafe` anywhere.
+      #
+      # @example
+      #
+      #   # bad
+      #   T.unsafe(foo)
+      #
+      #   # good
+      #   foo
+      class ForbidTUnsafe < RuboCop::Cop::Cop
+        def_node_matcher(:t_unsafe?, '(send (const nil? :T) :unsafe _)')
+
+        def on_send(node)
+          add_offense(node, message: "Do not use `T.unsafe`.") if t_unsafe?(node)
+        end
+      end
+    end
+  end
+end
