6313c18116bf4ad0a728a8c03c268164d6564cc9
Vinicius Stock <vinistock@users.noreply.github.com>
2021-10-21 16:01:37 -0400

    Mark callback binding cop as unsafe and disable it by default (#81)
    * Mark callback binding cop as unsafe and disable it by default
    
    * Fix typo
    
    Co-authored-by: Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
    
    * Fix typo in the docs too
    
    Co-authored-by: Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>

--- lib/rubocop/cop/sorbet/callback_conditionals_binding.rb
+++ lib/rubocop/cop/sorbet/callback_conditionals_binding.rb
@@ -6,6 +6,10 @@ module RuboCop
       # This cop ensures that callback conditionals are bound to the right type
       # so that they are type checked properly.
       #
+      # Auto-correction is unsafe because other libraries define similar style callbacks as Rails, but don't always need
+      # binding to the attached class. Auto-correcting those usages can lead to false positives and auto-correction
+      # introduces new typing errors.
+      #
       # @example
       #
       #   # bad
