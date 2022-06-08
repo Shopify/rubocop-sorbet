ca7b13d01e41d56131ae2f6ba4afc1023e3f823a
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-06-08 14:53:28 -0400

    add additional test cases

--- spec/rubocop/cop/sorbet/forbid_t_untyped_spec.rb
+++ spec/rubocop/cop/sorbet/forbid_t_untyped_spec.rb
@@ -5,10 +5,44 @@ require "spec_helper"
 RSpec.describe(RuboCop::Cop::Sorbet::ForbidTUntyped, :config) do
   subject(:cop) { described_class.new(config) }
 
-  it "adds offense when using T.untyped" do
-    expect_offense(<<~RUBY)
-      T.untyped
-      ^^^^^^^^^ Do not use `T.untyped`.
-    RUBY
+  context 'a simple usage' do
+    it 'adds an offense' do
+      expect_offense(<<~RUBY)
+        T.untyped
+        ^^^^^^^^^ Do not use `T.untyped`.
+      RUBY
+    end
+  end
+
+  context 'used within a type alias' do
+    it 'adds offense' do
+      expect_offense(<<~RUBY)
+        FOO = T.type_alias { T.untyped }
+                             ^^^^^^^^^ Do not use `T.untyped`.
+      RUBY
+    end
+  end
+
+  context 'used within a type signature' do
+    it 'adds offense' do
+      expect_offense(<<~RUBY)
+        sig { params(x: T.untyped).returns(T.untyped) }
+                                           ^^^^^^^^^ Do not use `T.untyped`.
+                        ^^^^^^^^^ Do not use `T.untyped`.
+        def foo(x)
+        end
+      RUBY
+    end
+  end
+
+  context 'used within T.bind' do
+    it 'adds offense' do
+      expect_offense(<<~RUBY)
+        def foo(x)
+          T.bind(self, Y::Array[T.untyped])
+                                ^^^^^^^^^ Do not use `T.untyped`.
+        end
+      RUBY
+    end
   end
 end
