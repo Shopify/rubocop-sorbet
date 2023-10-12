e74f99484bb684de9a2f256ab876c77791654a0e
Sam Bostock <sam.bostock@shopify.com>
2023-10-12 16:45:21 -0400

    Migrate from deprecated `Cop` to `Base` parent class

--- lib/rubocop/cop/sorbet/signatures/empty_line_after_sig.rb
+++ lib/rubocop/cop/sorbet/signatures/empty_line_after_sig.rb
@@ -18,27 +18,24 @@ module RuboCop
       #   sig { void }
       #   def foo; end
       #
-      class EmptyLineAfterSig < ::RuboCop::Cop::Cop # rubocop:todo InternalAffairs/InheritDeprecatedCopClass
+      class EmptyLineAfterSig < ::RuboCop::Cop::Base
+        extend AutoCorrector
         include RangeHelp
         include SignatureHelp
 
         def on_signature(node)
           if (next_method(node).line - node.last_line) > 1
             location = source_range(processed_source.buffer, next_method(node).line - 1, 0)
-            add_offense(node, location: location, message: "Extra empty line or comment detected")
-          end
-        end
-
-        def autocorrect(node)
-          ->(corrector) do
-            offending_range = node.source_range.with(
-              begin_pos: node.source_range.end_pos + 1,
-              end_pos: processed_source.buffer.line_range(next_method(node).line).begin_pos,
-            )
-            corrector.remove(offending_range)
-            clean_range = offending_range.source.split("\n").reject(&:empty?).join("\n")
-            offending_line = processed_source.buffer.line_range(node.source_range.first_line)
-            corrector.insert_before(offending_line, "#{clean_range}\n") unless clean_range.empty?
+            add_offense(location, message: "Extra empty line or comment detected") do |corrector|
+              offending_range = node.source_range.with(
+                begin_pos: node.source_range.end_pos + 1,
+                end_pos: processed_source.buffer.line_range(next_method(node).line).begin_pos,
+              )
+              corrector.remove(offending_range)
+              clean_range = offending_range.source.split("\n").reject(&:empty?).join("\n")
+              offending_line = processed_source.buffer.line_range(node.source_range.first_line)
+              corrector.insert_before(offending_line, "#{clean_range}\n") unless clean_range.empty?
+            end
           end
         end
 
