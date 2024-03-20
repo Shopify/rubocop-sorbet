5608a792a6dbe2043591b4409577ba774ac68162
Sam Bostock <sam.bostock@shopify.com>
2024-03-19 22:20:21 -0400

    Refactor `ForbidRBIOutsideOfAllowedPaths`
    This refactors the cop to inherit from `RuboCop::Cop::Base` instead of
    the deprecated `RuboCop::Cop::Cop` class.

--- lib/rubocop/cop/sorbet/rbi/forbid_rbi_outside_of_allowed_paths.rb
+++ lib/rubocop/cop/sorbet/rbi/forbid_rbi_outside_of_allowed_paths.rb
@@ -20,37 +20,31 @@ module RuboCop
       #   # rbi/external_interface.rbi
       #   # sorbet/rbi/some_file.rbi
       #   # sorbet/rbi/any/path/for/file.rbi
-      class ForbidRBIOutsideOfAllowedPaths < RuboCop::Cop::Cop # rubocop:todo InternalAffairs/InheritDeprecatedCopClass
+      class ForbidRBIOutsideOfAllowedPaths < RuboCop::Cop::Base
         include RangeHelp
 
-        def investigate(processed_source)
+        MSG = "RBI file path should match one of: %<allowed_paths>s"
+
+        def on_new_investigation
           paths = allowed_paths
 
+          # binding.irb
+          first_line_range = source_range(processed_source.buffer, 1, 0)
+
           if paths.nil?
-            add_offense(
-              nil,
-              location: source_range(processed_source.buffer, 1, 0),
-              message: "AllowedPaths expects an array",
-            )
+            add_offense(first_line_range, message: "AllowedPaths expects an array")
             return
           elsif paths.empty?
-            add_offense(
-              nil,
-              location: source_range(processed_source.buffer, 1, 0),
-              message: "AllowedPaths cannot be empty",
-            )
+            add_offense(first_line_range, message: "AllowedPaths cannot be empty")
             return
           end
 
           # When executed the path to the source file is absolute.
           # We need to remove the exec path directory prefix before matching with the filename regular expressions.
           rel_path = processed_source.file_path.sub("#{Dir.pwd}/", "")
+          return if paths.any? { |pattern| File.fnmatch(pattern, rel_path) }
 
-          add_offense(
-            nil,
-            location: source_range(processed_source.buffer, 1, 0),
-            message: "RBI file path should match one of: #{paths.join(", ")}",
-          ) if paths.none? { |pattern| File.fnmatch(pattern, rel_path) }
+          add_offense(first_line_range, message: format(MSG, allowed_paths: paths.join(", ")))
         end
 
         private
