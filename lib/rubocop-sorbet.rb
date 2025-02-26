c7039e5368c1cbd62e337d0d6e585e230da6929c
Ngan Pham <ngan@users.noreply.github.com>
2025-02-25 17:29:26 -0800

    Pluginfy RuboCop Performance

--- lib/rubocop-sorbet.rb
+++ lib/rubocop-sorbet.rb
@@ -4,8 +4,6 @@ require "rubocop"
 
 require_relative "rubocop/sorbet"
 require_relative "rubocop/sorbet/version"
-require_relative "rubocop/sorbet/inject"
-
-RuboCop::Sorbet::Inject.defaults!
+require_relative "rubocop/sorbet/plugin"
 
 require_relative "rubocop/cop/sorbet_cops"
