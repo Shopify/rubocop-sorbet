40e0b0b8a7f46f32f77822e05a5dcbb6b59e624a
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2025-03-25 19:42:20 +0200

    Do not load plugin class if RuboCop version is too old

--- lib/rubocop/sorbet.rb
+++ lib/rubocop/sorbet.rb
@@ -10,7 +10,7 @@ module RuboCop
   module Sorbet
     class Error < StandardError; end
 
-    unless Plugin::SUPPORTED
+    unless defined?(::RuboCop::Sorbet::Plugin)
       PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
       CONFIG_DEFAULT = PROJECT_ROOT.join("config", "default.yml").freeze
       CONFIG         = YAML.safe_load(CONFIG_DEFAULT.read).freeze
