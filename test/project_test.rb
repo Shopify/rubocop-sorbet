f675d783763b37a372c0f2b61226200ecbbe4b6b
Rafael Mendonça França <rafael.franca@shopify.com>
2025-05-12 18:51:13 +0000

    Make sure all cops are in the config

--- test/project_test.rb
+++ test/project_test.rb
@@ -4,6 +4,10 @@ require "test_helper"
 require "pathname"
 
 class ProjectTest < Minitest::Test
+  RBI_ONLY_COPS = [
+    "Sorbet/ValidGemVersionAnnotations",
+  ]
+
   def test_no_rogue_rubocop_comments
     root_directory = Pathname.new(File.expand_path("..", __dir__))
 
@@ -17,5 +21,28 @@ class ProjectTest < Minitest::Test
         "File #{relative_path} contains rogue rubocop comments",
       )
     end
+  end
+
+  def test_has_configuration_for_all_cops
+    assert_equal(code_cop_names.sort, cops_on(config))
+  end
+
+  def cop_names
+    @cop_names ||= RuboCop::Cop::Registry
+      .global
+      .select { |cop| cop.cop_name.start_with?("Sorbet/") }
+      .map(&:cop_name)
+  end
+
+  def code_cop_names
+    @code_cop_names ||= cop_names - RBI_ONLY_COPS
+  end
+
+  def config
+    @config ||= RuboCop::ConfigLoader.load_file("config/default.yml")
+  end
+
+  def cops_on(config)
+    config.keys.reject { |key| key == "inherit_mode" }.sort
   end
 end
