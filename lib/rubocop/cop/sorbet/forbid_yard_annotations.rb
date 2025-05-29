381cbe82e68b2b3937492b038d8632e52b48f66a
Brad Lindsay <brad.lindsay@shopify.com>
2025-05-29 15:10:28 -0500

    WIP: Insert a placeholder at end of the comment block

--- lib/rubocop/cop/sorbet/forbid_yard_annotations.rb
+++ lib/rubocop/cop/sorbet/forbid_yard_annotations.rb
@@ -26,6 +26,7 @@ module RuboCop
       #   end
       #
       class ForbidYardAnnotations < Base
+        include Alignment
         extend AutoCorrector
 
         MSG = "Avoid using YARD method annotations. Use RBS comment syntax instead."
@@ -46,29 +47,57 @@ module RuboCop
         def on_new_investigation
           return if processed_source.comments.empty?
 
-          yard_tag_blocks.each do |block|
-            next unless (tag_block = block.first)
-            next unless contains_forbidden_yard_tag?(tag_block.text)
+          comment_blocks.each do |comment_block|
+            forbidden_blocks = forbidden_yard_tag_blocks(comment_block)
+            forbidden_blocks.each_with_index do |tag_block, index|
+              add_offense(tag_block.first) do |corrector|
+                corrector.remove(range_of_lines_for(tag_block))
 
-            add_offense(tag_block) do |corrector|
-              corrector.remove(range_of_lines_for(block))
-              range_of_lines_for(block)
+                if index + 1 == forbidden_blocks.size
+                  corrector.insert_after(
+                    comment_block.last.source_range.end.resize(1).end,
+                    "#{offset(tag_block.first)}#: () -> void\n",
+                  )
+                end
+              end
             end
           end
         end
 
         private
 
-        def yard_tag_blocks
+        def comment_blocks
           Enumerator.new do |yielder|
             comments = processed_source.comments
             next if comments.empty?
 
-            current_tag_chunk = []
+            current_chunk = []
             previous_line = -1
-            tag_indent_level = 0
 
             comments.each do |comment|
+              if comment.location.line == previous_line + 1
+                current_chunk << comment
+              else
+                yielder << current_chunk unless current_chunk.empty?
+                current_chunk = [comment]
+              end
+
+              previous_line = comment.location.line
+            end
+
+            yielder << current_chunk unless current_chunk.empty?
+          end
+        end
+
+        def forbidden_yard_tag_blocks(comment_block)
+          return [] if comment_block.empty?
+
+          result = []
+          current_tag_chunk = []
+          previous_line = -1
+          tag_indent_level = 0
+
+          comment_block.each do |comment|
               scanner = StringScanner.new(comment.text)
               next unless scanner.skip("#")
 
@@ -79,10 +108,10 @@ module RuboCop
                   indent_level >= tag_indent_level + 2
                 current_tag_chunk << comment
               else
-                yielder << current_tag_chunk unless current_tag_chunk.empty?
+              result << current_tag_chunk unless current_tag_chunk.empty?
                 current_tag_chunk = []
 
-                if scanner.skip("@")
+              if scanner.skip("@") && forbidden_yard_tag_next?(scanner)
                   current_tag_chunk << comment
                   tag_indent_level = indent_level
                 end
@@ -91,18 +120,12 @@ module RuboCop
               previous_line = comment.location.line
             end
 
-            yielder << current_tag_chunk unless current_tag_chunk.empty?
-          end
+          result << current_tag_chunk unless current_tag_chunk.empty?
+
+          result
         end
 
-        def contains_forbidden_yard_tag?(comment_text)
-          scanner = StringScanner.new(comment_text)
-          return false unless scanner.skip("#")
-
-          scanner.skip(ANY_WHITESPACE)
-
-          return false unless scanner.skip("@")
-
+        def forbidden_yard_tag_next?(scanner)
           FORBIDDEN_YARD_TAGS.any? do |tag_name|
             if scanner.skip(tag_name)
               scanner.unscan unless (match = scanner.eos? || scanner.peek(1).lstrip.empty?)
