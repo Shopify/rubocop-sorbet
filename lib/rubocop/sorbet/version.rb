bfbe542e25c8884d6364ad194afed11f459c9d64
Stan Lo <stan.lo@shopify.com>
2025-06-25 21:38:18 +0100

    Update lib/rubocop/sorbet/version.rb
    Co-authored-by: Vinicius Stock <vinistock@users.noreply.github.com>

--- lib/rubocop/sorbet/version.rb
+++ lib/rubocop/sorbet/version.rb
@@ -2,6 +2,6 @@
 
 module RuboCop
   module Sorbet
-    VERSION = File.read(File.expand_path("../../../../VERSION", __FILE__)).strip
+    VERSION = File.read(File.expand_path("../../../VERSION", __dir__)).strip
   end
 end
